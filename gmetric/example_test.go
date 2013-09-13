27d661ee4c14d0a4009d72b8999f5316b290efff
Naitik Shah <n@daaku.org>
2013-09-12 22:41:14 -0700

    package example

---
+++ gmetric/example_test.go
@@ -0,0 +1,54 @@
+package gmetric_test
+
+import (
+	"fmt"
+	"net"
+	"os"
+	"time"
+
+	"github.com/daaku/go.ganglia/gmetric"
+)
+
+func Example() {
+	// A Client can connect to multiple addresses.
+	client := &gmetric.Client{
+		Addr: []net.Addr{
+			&net.UDPAddr{IP: net.ParseIP("127.0.0.1"), Port: 8649},
+		},
+	}
+
+	// You only need to Open the connections once on application startup.
+	if err := client.Open(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	// Defines the Metric.
+	metric := &gmetric.Metric{
+		Name:         "web_requests",
+		Title:        "Number of Web Requests",
+		Host:         "web0.app.com",
+		ValueType:    gmetric.ValueUint32,
+		Units:        "count",
+		Slope:        gmetric.SlopeBoth,
+		TickInterval: 20 * time.Second,
+		Lifetime:     24 * time.Hour,
+	}
+
+	// Meta packets don't need to be sent regularly.
+	if err := client.SendMeta(metric); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	if err := client.SendValue(metric, 1); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	// Close the connections before termination your application.
+	if err := client.Close(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
